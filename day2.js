8bb1ceb77dea8200f8e45311cb8f8d7f05ce2af7
songmiO <dhthd1060@gmail.com>
2021-11-15 23:17:08 +0900

    Modify : Delete command.

--- day2.js
+++ day2.js
@@ -20,18 +20,7 @@ function readLine() {
   return inputString[currentLine++];
 }
 
-/*
- * Complete the 'solve' function below.
- *
- * The function accepts following parameters:
- *  1. DOUBLE meal_cost
- *  2. INTEGER tip_percent
- *  3. INTEGER tax_percent
- */
-
-function solve(meal_cost, tip_percent, tax_percent) {
-  // Write your code here
-}
+function solve(meal_cost, tip_percent, tax_percent) {}
 
 function main() {
   const meal_cost = parseFloat(readLine().trim());
