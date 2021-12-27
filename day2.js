8526555ae7c6585b998ec1fa7be4f8a81342a704
songmiO <dhthd1060@gmail.com>
2021-12-27 22:43:08 +0900

    Add : Adding the console.

--- day2.js
+++ day2.js
@@ -1,33 +1,34 @@
-// 'use strict';
+'use strict';
 
-// process.stdin.resume();
-// process.stdin.setEncoding('utf-8');
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
 
-// let inputString = '';
-// let currentLine = 0;
+let inputString = '';
+let currentLine = 0;
 
-// process.stdin.on('data', function (inputStdin) {
-//   inputString += inputStdin;
-// });
+process.stdin.on('data', function (inputStdin) {
+  inputString += inputStdin;
+});
 
-// process.stdin.on('end', function () {
-//   inputString = inputString.split('\n');
+process.stdin.on('end', function () {
+  inputString = inputString.split('\n');
 
-//   main();
-// });
+  main();
+});
 
-// function readLine() {
-//   return inputString[currentLine++];
-// }
+function readLine() {
+  return inputString[currentLine++];
+}
 
-// function solve(meal_cost, tip_percent, tax_percent) {}
+function solve(meal_cost, tip_percent, tax_percent) {}
 
-// function main() {
-//   const meal_cost = parseFloat(readLine(1).trim());
+function main() {
+  const meal_cost = parseFloat(readLine(1).trim());
 
-//   const tip_percent = parseInt(readLine().trim(), 10);
+  const tip_percent = parseInt(readLine().trim(), 10);
 
-//   const tax_percent = parseInt(readLine().trim(), 10);
+  const tax_percent = parseInt(readLine().trim(), 10);
 
-//   solve(meal_cost, tip_percent, tax_percent);
-// }
+  solve(meal_cost, tip_percent, tax_percent);
+  console.log('출력');
+}
